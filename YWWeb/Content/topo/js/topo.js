79f5b28d8714d039cec89b1dbb7e17523529953a
wwn <1076681389@qq.com>
2018-11-29 10:27:19 +0800

    修改一次图展示功能

--- YWWeb/Content/topo/js/topo.js
+++ YWWeb/Content/topo/js/topo.js
@@ -3,6 +3,7 @@
 function Topo() {};
 Topo.prototype = {
     init: function() {
+
         this.pid = $("#pid").html();
         this.orderNo = $("#orderNo").html() || 1;
 
@@ -15,34 +16,40 @@ Topo.prototype = {
         this.scene.clear();
         $.ajax({
             type: "post",
-            url: "/PDRInfo/GetOneView",
+            url: "https://yw.ife360.com/PDRInfo/GetOneView",
             data: {
                 pid: that.pid,
                 orderNo: that.orderNo,
             },
             success: function(res) {
-                console.log(res)
                 $.ajax({
                     type: "get",
                     url: res + "?date" + new Date().valueOf(),
                     success: function(res) {
-                        that.history(res);
-                    },error: function(){
-                        console.log("有错")
+                        that.history(JSON.parse(res));
                     }
-
                 })
             },
 
         })
+
+    },
+    uncompileStr: function(code) {
+        code = unescape(code);
+        var c = String.fromCharCode(code.charCodeAt(0) - code.length);
+        for (var i = 1; i < code.length; i++) {
+            c += String.fromCharCode(code.charCodeAt(i) - c.charCodeAt(i - 1));
+        }
+        return c;
     },
     // 读取历史
     history: function(obj) {
-        $('[data-type=pid]').val(obj.config.pid);
-        $('[data-type=orderNo]').val(obj.config.orderNo);
-        $("[data-type=bgcolor]").val(obj.config.bgColor);
         this.scene.backgroundColor = obj.config.bgColor;
-        this.copyNode(obj.nodes)
+        this.__IP = this.uncompileStr(obj.config.IP);
+        this.__account = this.uncompileStr(obj.config.account);
+        this.__password = this.uncompileStr(obj.config.password);
+        this.__port = this.uncompileStr(obj.config.port);
+        this.copyNode(obj.nodes);
         this.getBindData();
     },
     //复制节点
@@ -137,19 +144,21 @@ Topo.prototype = {
                 $('#electricMeterBounced').show();
                 $('#electricMeterBounced').css({ "top": e.clientY + node.height / 3 + 'px', "left": e.clientX - 150 + 'px' });
                 that.matchElectricMeter(node._cid);
+                that.timer = setInterval(function() {
+                    that.matchElectricMeter(node._cid);
+                }, 1000)
             })
             node.mouseout(function() {
+                clearInterval(that.timer);
                 $('#electricMeterBounced').hide();
-                // $("#topo").css("cursor", "default");
             })
         }
-
         node.selected = false;
         node.dragable = false;
         node.editAble = false;
         node.showSelected = false;
 
-        this.scene.add(node)
+        this.scene.add(node);
     },
     //获取绑定节点的数据
     getBindData: function() {
@@ -162,34 +171,26 @@ Topo.prototype = {
             if (e._cid) {
                 electricMeter.push(e);
             }
-        })
+        });
         this.specialNode = attributeNode;
         this.electricMeter = electricMeter;
     },
-    //轮询请求节点状态
     getNodeState: function() {
         var that = this;
-
-
-
-
+        //等节点加载成功
         var timer1 = setInterval(function() {
             if (that.specialNode) {
-                clearInterval(timer1)
+                clearInterval(timer1);
                 $.ajax({
                     type: 'POST',
-                    url: "/PDRInfo/GetOneGraph_kg",
-                    data: {
-                        "pid": that.pid,
-                    },
+                    url: "https://yw.ife360.com/PDRInfo/GetOneGraph_kg",
+                    data: { "pid": that.pid, },
                     success: function(res) {
-
                         var data = JSON.parse(res);
                         var nodes = that.specialNode;
                         if (!nodes) {
                             return
                         }
-
                         for (var a = 0; a < nodes.length; a++) {
                             if (nodes[a].__type == "text") {
                                 that.nodeText(nodes[a], data);
@@ -205,12 +206,10 @@ Topo.prototype = {
         }, 150)
 
 
-
-
-        //初始化表数据
+        //第一次获取所有数据
         $.ajax({
             type: 'POST',
-            url: "/PDRInfo/GetOneGraph_value",
+            url: "https://yw.ife360.com/PDRInfo/GetOneGraph_value",
             data: {
                 "pid": that.pid,
             },
@@ -221,105 +220,102 @@ Topo.prototype = {
         })
 
 
-        //长连接
-        function mqtt() {
-            var wsbroker = "59.110.153.200";
-            location.hostname; //mqtt websocket enabled broker ip
-            var wsport = 15675; // 端口号
-            //连接选项
-            var client;
-            var options = {
-                timeout: 30,
-                userName: "webguest",
-                password: "!@#23&Qbn",
-                keepAliveInterval: 10,
-                onSuccess: function(e) {
-                    console.log(("连接成功"))
-                    client.subscribe('/ny/12', {
-                        qos: 2
-                    });
-                },
-                onFailure: function(message) {
-                    console.log("连接失败 " + message.errorMessage);
-                    setTimeout(() => {
-                        mqtt();
-                    }, 10000);
-                }
-            };
-            if (location.protocol == "https:") {
-                options.useSSL = true;
-            }
-            client = new Paho.MQTT.Client(wsbroker, wsport, "/ws", "myclientid_" + guid());
-            //创建连接
-            client.connect(options);
-            client.onConnectionLost = function(responseObject) {
-                if (responseObject.errorCode !== 0) {
-                    console.error("异常掉线，掉线信息为:" + responseObject.errorMessage);
-                }
-                setTimeout(() => {
-                    mqtt();
-                }, 10000);
-            };
-            client.onMessageArrived = function(res) {
-                var payload = JSON.parse(res.payloadString);
-                console.log(payload)
-                if (!payload.type) {
-                    return;
-                }
-                if (payload.type == 6) {
-                    var changeData = payload.content;
-                    for (var key in changeData) {
-                        for (var a = 0; a < that.meterData.length; a++) {
-                            if (key == that.meterData[a].TagID) {
-                                that.meterData[a].DValue = changeData[key].PV;
-                                break;
-                            }
-                        }
-                    }
-                    return
-                }
-                var content = payload.content;
-                var nodes = that.specialNode;
-                if (!nodes) {
-                    return
-                }
-                for (var a = 0; a < nodes.length; a++) {
-                    if (nodes[a].__type == "text") {
-                        that.nodeText(nodes[a], content);
-                    } else if (nodes[a].__type == "node") {
-                        that.nodeShape(nodes[a], content);
-                    } else if (nodes[a].__type == "line") {
-                        that.nodeLine(nodes[a], content);
-                    }
-                }
-
-
-            };
-        }
-
-        function guid() {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                var r = Math.random() * 16 | 0,
-                    v = c == 'x' ? r : (r & 0x3 | 0x8);
-                return v.toString(16);
-            });
-        }
-
-        mqtt();
-
-
-
-
+        //连接mqtt获取变化量
+        setTimeout(() => { that.mqtt(); }, 1000);
 
 
 
 
 
     },
+    guid: function() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    },
+    mqtt: function() {
+        var that = this;
+        var wsbroker = that.__IP;
+        location.hostname;
+        var wsport = parseInt(that.__port);
+        //连接选项
+        var client;
+        var options = {
+            timeout: 30,
+            userName: that.__account,
+            password: that.__password,
+            keepAliveInterval: 10,
+            onSuccess: function(e) {
+                console.log(("连接成功"))
+                client.subscribe('/ny/12', {
+                    qos: 2
+                });
+            },
+            onFailure: function(message) {
+                console.log("连接失败 " + message.errorMessage);
+                setTimeout(() => {
+                    that.mqtt();
+                }, 10000);
+            }
+        };
+        if (location.protocol == "https:") {
+            options.useSSL = true;
+        }
+        client = new Paho.MQTT.Client(wsbroker, wsport, "/ws", "myclientid_" + that.guid());
+        //创建连接
+        client.connect(options);
+        client.onConnectionLost = function(responseObject) {
+            if (responseObject.errorCode !== 0) {
+                console.error("异常掉线，掉线信息为:" + responseObject.errorMessage);
+            }
+            setTimeout(() => {
+                that.mqtt();
+            }, 10000);
+        };
+        client.onMessageArrived = function(res) {
+            var payload = JSON.parse(res.payloadString);
+            if (!payload.type) {
+                return;
+            }
+            if (payload.type == 6) {
+                var changeData = payload.content;
+                for (var key in changeData) {
+                    for (var a = 0; a < that.meterData.length; a++) {
+                        if (key == that.meterData[a].TagID) {
+                            that.meterData[a].DValue = changeData[key].PV;
+                            break;
+                        }
+                    }
+                }
+                return
+            }
+            var content = payload.content;
+            var nodes = that.specialNode;
+            if (!nodes) {
+                return
+            }
+            for (var a = 0; a < nodes.length; a++) {
+                if (nodes[a].__type == "text") {
+                    that.nodeText(nodes[a], content);
+                } else if (nodes[a].__type == "node") {
+                    that.nodeShape(nodes[a], content);
+                } else if (nodes[a].__type == "line") {
+                    that.nodeLine(nodes[a], content);
+                }
+            }
+
+
+        };
+    },
     // 文字类型改变
     nodeText: function(node, data) {
-        var pv = data[node.id].PV;
-        node.text = parseFloat(pv);
+        if (data[node.id]) {
+            var pv = data[node.id].PV;
+            node.text = parseFloat(pv);
+        }
+
     },
     // 节点类型改变
     nodeShape: function(node, data) {
@@ -329,12 +325,13 @@ Topo.prototype = {
         if (node._failureState && data[node._failureState]) {
             pv1 = data[node._failureState].PV;
         }
-
         if (node._parentID) {
             var arr = node._parentID.split(",");
             var num = 0;
             for (var a = 0; a < arr.length; a++) {
-                num += data[arr[a]].PV;
+                if (data[arr[a]]) {
+                    num += data[arr[a]].PV;
+                }
             }
             if (arr.length >= 3 && num >= 2) {
                 pv2 = 1;
@@ -344,23 +341,19 @@ Topo.prototype = {
                 pv2 = 0;
             }
         }
-
-
-
-
-        if (node.id) {
+        if (node.id && data[node.id]) {
             pv3 = data[node.id].PV;
         }
         if (pv1 == 1) {
             node.color = node.state2;
         } else if (pv2 == 0) {
-            node.color = node.state1;
+            node.color = node.state0;
         } else if (pv2 == 1 && pv3 == undefined) {
-            node.color = node.state0;
-        } else if ((pv3 == 1 && pv2 == 1) || (pv3 == 1 && pv2 == undefined)) {
-            node.color = node.state0;
-        } else {
             node.color = node.state1;
+        } else if ((pv3 == 1 && pv2 == 1) || (pv3 == 1 && pv2 == undefined)) {
+            node.color = node.state1;
+        } else {
+            node.color = node.state0;
         }
     },
     // 线类型改变
@@ -368,50 +361,38 @@ Topo.prototype = {
         var arr = node._parentID.split(",");
         var num = 0;
         for (var a = 0; a < arr.length; a++) {
-
-            num += data[arr[a]].PV;
+            if (data[arr[a]]) {
+                num += data[arr[a]].PV;
+            }
         }
         if (arr.length >= 3 && num >= 2) {
-            node.fillColor = node.state0;
-        } else if (arr.length <= 2 && num >= 1) {
-            node.fillColor = node.state0;
-        } else {
             node.fillColor = node.state1;
+        } else if (arr.length <= 2 && num >= 1) {
+            node.fillColor = node.state1;
+        } else {
+            node.fillColor = node.state0;
         }
     },
     // 匹配表
     matchElectricMeter: function(cid) {
         var component = [];
         var datatypeid = -1;
-        for (var a = 0; a < this.meterData.length; a++) {
+        for (var a = 0, b = 0; a < this.meterData.length; a++) {
             var meter = this.meterData[a];
             if (meter.CID == cid) {
-
-                if (component.length == 0) {
-                    component.push(`
-                    </tr><tr><th colspan="4">${meter.CName}</th><tr>
-                    <tr><td width="140px"></td><td>A</td><td>B</td><td>C</td>
-                `)
+                if (b == 0) {
+                    $('.title').html(meter.CName);
+                    b++;
                 }
-                if (meter.DataTypeID != datatypeid) {
-                    datatypeid = meter.DataTypeID;
-                    component.push(`
-                        </td><tr><td>${meter.CtypeName}${meter.Units}</td>
-                    `)
-                }
-                if (meter.DataTypeID == 2 || meter.DataTypeID == 3) {
-                    component.push(`
-                    <td>${ meter.DValue}</td>
-                `)
+                if (meter.CtypeName == '电流') {
+                    $('.I' + meter.TagName[meter.TagName.length - 1]).html(meter.DValue);
+                } else if (meter.CtypeName == '电压') {
+                    $('.UA' + meter.TagName[meter.TagName.length - 1]).html(meter.DValue);
                 } else {
-                    component.push(`
-                    <td colspan="3">${ meter.DValue}</td>
-                `)
+                    $('.' + meter.TagName[meter.TagName.length - 1]).html(meter.DValue);
                 }
-
             }
         }
-        $("#electricMeterBounced").html(component.join(""));
     }
 };
 var topo = new Topo();
