bb1b134f7a3ebaeea364b5b9db5d0a245f2ac01e
jiang <jiang@email.com>
2018-12-28 09:27:31 +0800

    添加文件

---
+++ YWWeb.Lib/EnergyManage/PubClasses/UserControllerBaseEx.cs
@@ -0,0 +1,25 @@
+﻿using Loger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YWWeb.Lib.Base;
+
+namespace EnergyManage.PubClass
+{
+    public class UserControllerBaseEx:UserControllerBase
+    {
+        public override void LogDebug(string msg)
+        {
+            Loger.LogHelper.Debug(msg);
+        }
+        public override void LogInfo(string msg)
+        {
+            LogHelper.Info(msg);
+        }
+        public override void LogException(Exception ex)
+        {
+            LogHelper.Error(ex);
+        }
+    }
+}
