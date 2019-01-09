df28384744e97424f0bfe812d1429de1cdaaed2c
Windwos 10IHQYE\Administrator <17600102048@163.com>
2019-01-09 18:18:08 +0800

    接口

---
+++ DBProject/DAL/CircuitInfoDAL.cs
@@ -0,0 +1,48 @@
+﻿using DAO;
+using IDAO;
+using IDAO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CircuitInfoDAL
+    {
+        IDBFactory _dbFactory = DBFactoryManager.GetDBFactory();
+        IDBCacheFactory _dbCacheFactory = DBCacheFactoryManager.GetDBFactory();
+
+        static CircuitInfoDAL _DataDal;
+        static readonly object _loker = new object();
+        public static CircuitInfoDAL getInstance(string json = null)
+        {
+            if (null == _DataDal)
+            {
+                lock (_loker)
+                {
+                    if (null == _DataDal)
+                        _DataDal = new CircuitInfoDAL();
+                }
+            }
+
+            return _DataDal;
+        }
+        public IList<t_DM_CircuitInfo> GetCID(string cids,int type)
+        {
+            IList<t_DM_CircuitInfo> data = new List<t_DM_CircuitInfo>();
+            try
+            {
+                data = _dbFactory.circuitinfo.GetCID(cids, type);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return data;
+
+        }
+    }
+}
---
+++ DBProject/DAO/MSSql/CircuitInfoDBContext.cs
@@ -0,0 +1,32 @@
+﻿using IDAO;
+using IDAO.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class CircuitInfoDBContext : DBContextBase, ICircuitInfo
+    {
+        public CircuitInfoDBContext()
+             : base("name=YWConnectionStringHis")
+        {
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<CircuitInfoDBContext>(null);
+            modelBuilder.Entity<t_DM_CircuitInfo>()
+              .HasKey(t => new { t.ID });
+            base.OnModelCreating(modelBuilder);
+        }
+        public IList<t_DM_CircuitInfo> GetCID(string cids, int type)
+        {
+            string sql = $@"select c.ID, CID,c.Name from (select a.ID,CID,b.Name,a.coolect_dev_type from [t_DM_CircuitInfo] a join [t_EE_EnerUserType] b on a.ener_use_type=b.id) as c 
+                        where c.CID in({cids}) and coolect_dev_type={type}";
+            return SQLQuery<t_DM_CircuitInfo>(sql);
+        }
+    }
+}
