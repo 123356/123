ae7effbc7a328c426e1c215a535d26fc5986fd35
wwn <wwn.com>
2019-03-11 12:12:39 +0800

    接口

--- DBProject/IDAO/IVEnerProjectType.cs
+++ DBProject/IDAO/IVEnerProjectType.cs
@@ -10,12 +10,13 @@ namespace IDAO
     public interface IVEnerProjectType : IDAOBase, IDisposable
     {
         IList<t_V_EnerProjectType> GetTreeData(int unitID, int item_type);
-
-        IList<t_V_EnerPower> GetTreeData1(int unitID, int item_type);
         IList<t_V_EnerProjectType> UpdateRelationship(int child_id, int parent_id, int unit_id, string unit_head, string unit_note, string addCid, string delCid,int updateTypeID,int unit_area,int unit_people);
         IList<t_V_EnerProjectType> AddProjectTemplate(int unitID, int item_type);
         IList<t_V_EnerProjectType> GetHistoryList(int unitID, int item_type);
-        IList<t_V_EnerProjectTypePower> GetCidsToElectricity(string pids, string cids);
-        IList<t_V_EnerProjectTypePower> GetElectrMonth(string pid, string cid);
+
+
+        IList<t_V_EnerPower> GetElectricityToDay(string pid, string cid);
+        IList<t_V_EnerPower> GetElectricityToMonth(string pid, string cid);
+        
     }
 }
