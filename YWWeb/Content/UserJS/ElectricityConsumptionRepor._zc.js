794a1649885d6735ac6f961c09b757a9cdbde5b1
wanghui99 <735905079@qq.com>
2019-03-12 13:37:41 +0800

    用电报表格式

--- YWWeb/Content/UserJS/ElectricityConsumptionRepor._zc.js
+++ YWWeb/Content/UserJS/ElectricityConsumptionRepor._zc.js
@@ -113,7 +113,20 @@
                 })
                 .finally(function () {
                     that.loading = false
+                    that.setHeight()
                 })
+        },
+        setHeight: function () {
+            $(".pHeight").each(function () {
+                var pHeight = $(this).context.clientHeight
+                var cHeight = $(this).parent().find(".cHeight").height()
+                if (pHeight > cHeight) {
+                    $(this).parent().find(".cHeight").height(pHeight + 1)
+                } else {
+                    $(this).parent().find(".cHeight").css("height", "100%")
+                }
+
+            })
         },
         totalCom: function (data) {
             var dayTotal = 0
@@ -235,5 +248,6 @@
 
     },
     created: function () {
+        
     }
 })
